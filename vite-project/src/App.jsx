c923aded9cb90e020a5162a4a298245e83e5ac9c
Rana-Mubashir <mubashirsuperior138@gmail.com>
2024-12-18 22:28:54 -0800

    setup ready

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p className='text-red-500  text-4xl'>hello i am here starting a new blog app project</p>
+    </div>
+  )
+}
+
+export default App
